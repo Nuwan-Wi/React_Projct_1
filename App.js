8b01e23d07c787f8b00384c87cbac00361c1ecf5
Nuwan-Wi <nuwanwijeweera97@gmail.com>
2022-04-08 23:38:59 +0530

    Design Home Page

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <ImageBackground source={require('./assets/Group17.png')} style={styles.container}>
-      <Text>Hello my name is Nuwan Wijeweera</Text>
-      <StatusBar style="auto" />
-    </ImageBackground>
+    <SafeAreaView style={{ flex: 1 }}>
+      <ImageBackground source={ require('./assets/Group17.png')} resizeMode='stretch' style={styles.container}>
+        <View style={styles.heder}></View>
+        <Text>Hello my name is Nuwan Wijeweera</Text>
+        <StatusBar style="auto" />
+      </ImageBackground>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  
   },
+
+  heder: {
+  marginTop: 40,
+  height: 60,
+  width: 400,
+  backgroundColor: '#fff',
+  alignItems: 'center',  
+  }
 });
